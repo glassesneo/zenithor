b0419fb4b3a1ebb94a416b5c897f8b0b47d86c7d
neo <glassesneo@protonmail.com>
2025-09-18 22:39:28 +0900

    chore: update examples

--- examples/window.zig
+++ examples/window.zig
@@ -1,6 +1,10 @@
 const zenithor = @import("zenithor");
 
 pub fn main() !void {
-    var app = zenithor.Application.init();
-    app.run();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    var app = zenithor.Application.init(allocator);
+    try app.run();
 }
+
+const std = @import("std");
