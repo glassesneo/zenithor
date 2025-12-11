4a22ed1251a0678d15d722a732d0a9f419ce06cc
neo <glassesneo@protonmail.com>
2025-12-12 00:21:39 +0900

    feat: add shader support for opengl backend

--- build.zig
+++ build.zig
@@ -147,10 +147,12 @@ fn prepareBuildSetup(b: *Build) !BuildSetup {
         .input = "plugins/graphics/src/shader.glsl",
         .output = "pbr_shader.zig",
         .slang = .{
-            .glsl430 = true,
+            .glsl410 = true,
             .glsl300es = true,
             .hlsl5 = true,
             .metal_macos = true,
+            .metal_ios = true,
+            .metal_sim = true,
             .wgsl = true,
         },
     });
@@ -161,10 +163,12 @@ fn prepareBuildSetup(b: *Build) !BuildSetup {
         .input = "plugins/graphics/src/blinn_phong.glsl",
         .output = "blinn_phong_shader.zig",
         .slang = .{
-            .glsl430 = true,
+            .glsl410 = true,
             .glsl300es = true,
             .hlsl5 = true,
             .metal_macos = true,
+            .metal_ios = true,
+            .metal_sim = true,
             .wgsl = true,
         },
     });
@@ -175,10 +179,12 @@ fn prepareBuildSetup(b: *Build) !BuildSetup {
         .input = "plugins/graphics/src/unlit.glsl",
         .output = "unlit_shader.zig",
         .slang = .{
-            .glsl430 = true,
+            .glsl410 = true,
             .glsl300es = true,
             .hlsl5 = true,
             .metal_macos = true,
+            .metal_ios = true,
+            .metal_sim = true,
             .wgsl = true,
         },
     });
